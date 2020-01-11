9d69ac9cbcd82475197b52135527ab86d2a71426
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2020-01-11 13:05:57 -0300

    Leandro

---
+++ uri/1176.py
@@ -0,0 +1,13 @@
+def fib(n):
+    vetor = []
+    vetor.append(0)
+    vetor.append(1)
+    for i in range(2,n+1,1):
+        vetor.append(vetor[i-1] + vetor[i-2])
+    print(vetor)
+    return vetor[n]
+
+n = int (input())
+for i in range (n):
+  num = int(input())
+  print ("Fib({}) = {}".format(num,fib(num)))
